6160ca0d4345888b89d638df2ebe2c744e08a873
Kieran815 <kieran.milligan@gmail.com>
2020-09-17 17:45:01 -0500

    Updates

---
+++ precache-manifest.98cd1664d28b3dd2e140b398c9bb1407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548ad39bf104453f9788439e42dabcdb",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "f3fe799574b907f67b07",
+    "url": "/memory-game/static/css/main.5dec72d6.chunk.css"
+  },
+  {
+    "revision": "689649a47605e069e48a",
+    "url": "/memory-game/static/js/2.a3fab9b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.a3fab9b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3fe799574b907f67b07",
+    "url": "/memory-game/static/js/main.9670afd4.chunk.js"
+  },
+  {
+    "revision": "edbde91f6abb6e69834c",
+    "url": "/memory-game/static/js/runtime-main.a1f811c6.js"
+  }
+]);
